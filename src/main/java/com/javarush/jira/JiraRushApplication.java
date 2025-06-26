10b8c9d8e7c1802c10fa368732a05fc5d022a364
Buga Dan <danbil.942@gmail.com>
2025-06-26 13:17:12 +0300

    Added dotenv-java

--- src/main/java/com/javarush/jira/JiraRushApplication.java
+++ src/main/java/com/javarush/jira/JiraRushApplication.java
@@ -1,6 +1,7 @@
 package com.javarush.jira;
 
 import com.javarush.jira.common.internal.config.AppProperties;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,6 +13,11 @@ import org.springframework.cache.annotation.EnableCaching;
 public class JiraRushApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .load();
+
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(JiraRushApplication.class, args);
     }
 }
